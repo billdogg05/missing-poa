4aa898a00375d62adc846327999691805f984094
Bunyod Aminov <aminov.bun@gmail.com>
2026-06-24 15:31:43 +0500

    init: react app with sass

---
+++ client/src/App.jsx
@@ -0,0 +1,21 @@
+
+
+function App() {
+
+  return (
+    <>
+      <h1>test</h1>
+      <h2>test</h2>
+      <h3>test</h3>
+      <h4>test</h4>
+      <p>test</p>
+      <a href="#">test</a>
+      <p className="highlight">test</p>
+      <label htmlFor="test">test</label>
+      <input type="text" placeholder="test" id="test"/>
+
+    </>
+  )
+}
+
+export default App
